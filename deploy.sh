30e83cf2eb9b77d29446c9c1178a88a13b7c467c
jereme <jeremejazz15@gmail.com>
2018-07-06 22:43:47 +0800

    adjust to new repository

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ read -p "Press enter to continue"
 # Commit and push to master
 # Commit and push to master
 git add -A
-git commit -m "$msg" && git push origin hugo
+git commit -m "$msg" && git push origin master
 
 # Push the public subtree to the gh-pages branch
 git subtree push --prefix=public git@github.com:jeremejazz/jeremejazz.github.com.git master
