9a3e8373affccc4f77fe8e65ab34fb3ebc7d30a9
jereme <jeremejazz15@gmail.com>
2018-07-06 23:08:43 +0800

    theme upgrade

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,10 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 
+# Commit and push to master
+git add -A
+git commit -m "$msg" && git push origin master
+
 
 echo "Starting deployment... press enter to Continue. CTRL + C to cancel"
 read -p "Press enter to continue"
@@ -27,10 +31,6 @@ read -p "Press enter to continue"
 # Push the public subtree to the gh-pages branch
 git subtree push --prefix=public git@github.com:jeremejazz/jeremejazz.github.com.git master
  
-rm -rf public 
-
-# Commit and push to master
-git add -A
-git commit -m "$msg" && git push origin master
+# rm -rf public 
 
 #enter credentials here  
--- deployhttps.sh
+++ deployhttps.sh
@@ -20,15 +20,14 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 
+# Commit and push to master
+git add -A
+git commit -m "$msg" && git push origin master
+
 
 echo "Starting deployment... press enter to Continue. CTRL + C to cancel"
 read -p "Press enter to continue"
  
 # Push the public subtree to the gh-pages branch
 git subtree push --prefix=public https://github.com/jeremejazz/jeremejazz.github.com.git master
-rm -rf public/ 
-
-# Commit and push to master
-git add -A
-git commit -m "$msg" && git push origin master
 
