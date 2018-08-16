909d061b76486deab073ee306e2325e211129da5
jeremejazz <jeremejazz15@gmail.com>
2018-08-16 20:46:54 +0800

    fix page errors

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,5 @@
 
 
 // set fixed viewport
-$("meta[name='viewport']").attr("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0");
+//$("meta[name='viewport']").attr("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0");
 
